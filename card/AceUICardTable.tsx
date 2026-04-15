e092122d748abb697a7b192c74b83d9249765296
Fabian Ananda Merdana <yanappuowo@yandex.com>
2026-04-15 17:57:19 +0700

    feat: add AceUICardGraphs component and update AceUICardTable with dynamic titles and improved styling

--- card/AceUICardTable.tsx
+++ card/AceUICardTable.tsx
@@ -12,37 +12,43 @@ export type Tbody = {
 type Props = {
   thead: Thead[];
   tbody: Tbody[];
+  title: string;
 };
 
-function AceUICardTable({ thead = [], tbody = [] }: Props) {
+function AceUICardTable({ thead, tbody, title }: Props) {
   return (
     <>
-      <AceUICardWithTitle title="Grafik Tinggi Air">
-        <table className="w-full text-left border-collapse">
-          <thead>
-            <tr className="border-b">
-              {thead.map((item, index) => (
-                <th key={index} className="px-4 py-3 font-medium text-gray-700">
-                  {item.title}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {tbody.map((row) => {
-              const { id, ...dataCells } = row;
-              return (
-                <tr key={id} className="border-b last:border-b-0">
-                  {Object.values(dataCells).map((cellValue, cellIndex) => (
-                    <td key={cellIndex} className="px-4 py-3 text-gray-600">
-                      {cellValue}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <AceUICardWithTitle title={title}>
+        <div className="overflow-x-scroll md:overflow-hidden rounded-2xl border border-blue-100">
+          <table className="w-full text-left border-collapse">
+            <thead className="bg-blue-200 text-blue-900">
+              <tr>
+                {thead.map((item, index) => (
+                  <th key={index} className="px-4 py-3 font-medium">
+                    {item.title}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="bg-blue-50">
+              {tbody.map((row, rowIndex) => {
+                const { id, ...dataCells } = row;
+                return (
+                  <tr
+                    key={rowIndex}
+                    className="border-b border-blue-100 last:border-b-0"
+                  >
+                    {Object.values(dataCells).map((cellValue, cellIndex) => (
+                      <td key={cellIndex} className="px-4 py-3 text-gray-700">
+                        {cellValue}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </AceUICardWithTitle>
     </>
   );
